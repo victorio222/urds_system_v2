b4dd4690f50c573a1caa89cbd28bbeea68cff65a
cabatinganvictorio <cabatinganvictorio@gmail.com>
2025-12-16 03:27:52 +0800

    n

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/urds',
+  async rewrites() {
+    return [
+      {
+        source: 'api/:path*',
+        destination: 'https://urds-system-backendv2.onrender.com/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
