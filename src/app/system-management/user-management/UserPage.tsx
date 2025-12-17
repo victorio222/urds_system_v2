70fdd6d0b527b8f6e93210cfb81a38d5003ddef2
cabatinganvictorio <cabatinganvictorio@gmail.com>
2025-12-17 13:56:17 +0800

    ud

--- src/app/system-management/user-management/UserPage.tsx
+++ src/app/system-management/user-management/UserPage.tsx
@@ -14,7 +14,7 @@ const UserManagement = () => {
   return (
     <div className="flex max-h-screen">
       <div className="bg-blue-50 w-full">
-        <div className="px-8 py-4 space-y-2">
+        <div className="px-4 md:px-8 py-4 space-y-2">
           {/* ⭐ Responsive Grid for 8 TotalUser Cards (NO ARRAY MAP) */}
           <div
             className="
